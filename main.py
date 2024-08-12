61063ce35f05d474c6b692569992e3328764c8e7
YutoOkawa <yuutoowata.0305@gmail.com>
2024-08-12 17:14:00 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from wildCounter import WildCounter
+
+if __name__ == "__main__":
+    wc = WildCounter
+    wc.start()
---
+++ wildCounter.py
@@ -0,0 +1,7 @@
+class WildCounter:
+    def __init__(self):
+        pass
+
+    def start():
+        print("WildCounter started")
+        pass
